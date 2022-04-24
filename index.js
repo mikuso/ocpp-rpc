d67bd1f4df7edd860cab6d7b5c81323a5f98f0f0
Gareth Hughes <g_github@flamescape.com>
2022-04-24 02:47:42 +0100

    expose createRPCError

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const RPCClient = require('./lib/client');
 const RPCServer = require('./lib/server');
 const errors = require('./lib/errors');
+const { createRPCError } = require('./lib/util');
 
 module.exports = {
     RPCServer,
     RPCClient,
     ...errors,
+    createRPCError,
 };
