0ea2c924073d840ce1d513adc30c102aeb2d2eed
Gareth Hughes <g_github@flamescape.com>
2024-03-10 20:42:05 +0000

    wip: jsdocs

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import RPCClient from './lib/client';
-import RPCServer from './lib/server';
-import errors from './lib/errors';
-import symbols from './lib/symbols';
-import { createRPCError } from './lib/util';
-import { createValidator } from './lib/validator';
+import RPCClient from './lib/client.js';
+import RPCServer from './lib/server.js';
+import errors from './lib/errors.js';
+import symbols from './lib/symbols.js';
+import { createRPCError } from './lib/util.js';
+import { createValidator } from './lib/validator.js';
 
 export default {
     RPCServer,
